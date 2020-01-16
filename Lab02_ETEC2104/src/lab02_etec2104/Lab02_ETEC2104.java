25a836330fd86deb0a69fa06fe2eab969767e138
TehJellyLord <55899448+TehJellyLord@users.noreply.github.com>
2020-01-16 15:32:41 -0500

    Added a print line

--- Lab02_ETEC2104/src/lab02_etec2104/Lab02_ETEC2104.java
+++ Lab02_ETEC2104/src/lab02_etec2104/Lab02_ETEC2104.java
@@ -16,6 +16,7 @@ public class Lab02_ETEC2104 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Howdy Pilgrim");
     }
     
 }
