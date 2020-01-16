8bcf76b3f41eba5479ade571218985150c039dbc
TehJellyLord <55899448+TehJellyLord@users.noreply.github.com>
2020-01-16 15:46:39 -0500

    Changed the print line

--- Lab02_ETEC2104/src/lab02_etec2104/Lab02_ETEC2104.java
+++ Lab02_ETEC2104/src/lab02_etec2104/Lab02_ETEC2104.java
@@ -16,7 +16,7 @@ public class Lab02_ETEC2104 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Howdy Pilgrim");
+        System.out.println("Howdy Pilgrim! I'll be your tour guide!");
     }
     
 }
